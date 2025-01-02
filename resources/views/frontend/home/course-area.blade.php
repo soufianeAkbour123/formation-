195cacb73e939a9acae92737a0ce10b486e5f634
Nisrine HAMER <nisrinehamerr@gmail.com>
2025-01-02 01:36:46 +0100

    Update: COURSES

--- resources/views/frontend/home/course-area.blade.php
+++ resources/views/frontend/home/course-area.blade.php
@@ -260,87 +260,94 @@
                     </div>
                 </div>
 
-                @foreach ($categories as $category) 
-                <div class="tab-pane fade" id="business{{ $category->id }}" role="tabpanel" aria-labelledby="business-tab">
-                    <div class="row">
-                        @php
-                            $catwiseCourse = App\Models\Course::where('category_id',$category->id)->where('status',1)->orderBy('id','DESC')->get();
-                        @endphp                      
-                        
-                        @forelse ($catwiseCourse as $course)
-                        @php
-                            $amount = $course->selling_price - $course->discount_price;
-                            $discount = ($amount/$course->selling_price) * 100;
-                            $reviewcount = App\Models\Review::where('course_id',$course->id)->where('status',1)->latest()->get();
-                            $avarage = App\Models\Review::where('course_id',$course->id)->where('status',1)->avg('rating');
-                        @endphp
-                        <div class="col-lg-4 responsive-column-half position-relative">
-                            <div class="card card-item card-preview" data-tooltip-content="#tooltip_content_1{{ $course->id }}">
-                                <div class="card-image">
-                                    <a href="{{ url('course/details/'.$course->id.'/'.$course->course_name_slug) }}" class="d-block">
-                                        <img class="card-img-top lazy" src="{{ asset('public/upload/course/thambnail/' . basename($course->course_image)) }}" data-src="images/img8.jpg" alt="Card image cap">
-                                    </a>
-                                    <div class="course-badge-labels">
-                                        @if ($course->bestseller == 1)
-                                            <div class="course-badge">Meilleure Vente</div>
-                                        @endif
+               <!-- Mise à jour de la section des catégories -->
+@foreach ($categories as $category) 
+<div class="tab-pane fade" id="business{{ $category->id }}" role="tabpanel" aria-labelledby="business-tab">
+    <div class="row">
+        @php
+            $catwiseCourse = App\Models\Course::where('category_id',$category->id)->where('status',1)->orderBy('id','DESC')->get();
+        @endphp                      
+        
+        @forelse ($catwiseCourse as $course)
+        <div class="col-lg-4 responsive-column-half position-relative">
+            <div class="card card-item card-preview" data-tooltip-content="#tooltip_content_1{{ $course->id }}">
+                <div class="card-image">
+                    <a href="{{ url('course/details/'.$course->id.'/'.$course->course_name_slug) }}" class="d-block">
+                        <img class="card-img-top lazy" src="{{ asset('public/upload/course/thambnail/' . basename($course->course_image)) }}" data-src="images/img8.jpg" alt="Card image cap">
+                    </a>
 
-                                        @if ($course->highestrated == 1)
-                                            <div class="course-badge sky-blue">Le Mieux Noté</div>
-                                        @endif
+                    @php
+                        $amount = $course->selling_price - $course->discount_price;
+                        $discount = ($amount/$course->selling_price) * 100;
+                    @endphp
 
-                                        @if ($course->discount_price == NULL)
-                                            <div class="course-badge blue">Nouveau</div>
-                                        @else
-                                            <div class="course-badge blue">{{ round($discount) }}%</div>
-                                        @endif
-                                    </div>
-                                </div>
-                                
-                                <div class="card-body">
-                                    <h6 class="ribbon ribbon-blue-bg fs-14 mb-3">
-                                        @if ($course->label == 'Begginer')
-                                            Débutant
-                                        @elseif ($course->label == 'Middle')
-                                            Intermédiaire
-                                        @elseif ($course->label == 'Advance')
-                                            Avancé
-                                        @else
-                                            {{ $course->label }}
-                                        @endif
-                                    </h6>
-                                    <h5 class="card-title">
-                                        <a href="{{ url('course/details/'.$course->id.'/'.$course->course_name_slug) }}">{{ $course->course_name }}</a>
-                                    </h5>
-                                    <p class="card-text">
-                                        <a href="{{ route('instructor.details',$course->instructor_id) }}">{{ $course['user']['name'] }}</a>
-                                        @if ($course->discount_price != NULL)
-                                            <span class="text-success ml-2">-{{ round($discount) }}%</span>
-                                        @endif
-                                    </p>
+                    <div class="course-badge-labels">
+                        @if ($course->bestseller == 1)
+                            <span class="course-badge bestseller-badge">
+                                <i class="la la-gem"></i>
+                                <span class="badge-tooltip">Meilleure vente</span>
+                            </span>
+                        @endif
 
-                                    <div class="rating-wrap d-flex align-items-center py-2">
-                                        <div class="review-stars">
-                                            <span class="rating-number">{{ round($avarage,1) }}</span>
-                                            @if ($avarage == 0)
-                                                <span class="la la-star-o"></span>
-                                                <span class="la la-star-o"></span>
-                                                <span class="la la-star-o"></span>
-                                                <span class="la la-star-o"></span>
-                                                <span class="la la-star-o"></span>
-                                            @elseif ($avarage == 1 || $avarage < 2)
-                                                <span class="la la-star"></span>
-                                                <span class="la la-star-o"></span>
-                                                <span class="la la-star-o"></span>
-                                                <span class="la la-star-o"></span>
-                                                <span class="la la-star-o"></span>
-                                            @elseif ($avarage == 2 || $avarage < 3)
-                                                <span class="la la-star"></span>
-                                                <span class="la la-star"></span>
-                                                <span class="la la-star-o"></span>
-                                                <span class="la la-star-o"></span>
-                                                <span class="la la-star-o"></span>
-                                                @elseif ($avarage == 3 || $avarage < 4)
+                        @if ($course->discount_price == NULL)
+                            <span class="course-badge new-badge">
+                                <i class="la la-star"></i>
+                                <span class="badge-tooltip">Nouveau</span>
+                            </span>
+                        @endif
+                    </div>
+                </div>
+
+                @php
+                    $reviewcount = App\Models\Review::where('course_id',$course->id)->where('status',1)->latest()->get();
+                    $avarage = App\Models\Review::where('course_id',$course->id)->where('status',1)->avg('rating');
+                @endphp   
+
+                <div class="card-body">
+                    <h6 class="ribbon ribbon-blue-bg fs-14 mb-3">
+                        @if ($course->label == 'Begginer')
+                            Débutant
+                        @elseif ($course->label == 'Middle')
+                            Intermédiaire
+                        @elseif ($course->label == 'Advance')
+                            Avancé
+                        @else
+                            {{ $course->label }}
+                        @endif
+                    </h6>
+                    
+                    <h5 class="card-title">
+                        <a href="{{ url('course/details/'.$course->id.'/'.$course->course_name_slug) }}">{{ $course->course_name }}</a>
+                    </h5>
+                    <p class="card-text">
+                        <a href="{{ route('instructor.details',$course->instructor_id) }}">{{ $course['user']['name'] }}</a>
+                        @if ($course->discount_price != NULL)
+                            <span class="text-success ml-2">-{{ round($discount) }}%</span>
+                        @endif
+                    </p>
+
+                    <div class="rating-wrap d-flex align-items-center py-2">
+                        <div class="review-stars">
+                            <span class="rating-number">{{ round($avarage,1) }}</span>
+                            @if ($avarage == 0)
+                                <span class="la la-star-o"></span>
+                                <span class="la la-star-o"></span>
+                                <span class="la la-star-o"></span>
+                                <span class="la la-star-o"></span>
+                                <span class="la la-star-o"></span>
+                            @elseif ($avarage == 1 || $avarage < 2)
+                                <span class="la la-star"></span>
+                                <span class="la la-star-o"></span>
+                                <span class="la la-star-o"></span>
+                                <span class="la la-star-o"></span>
+                                <span class="la la-star-o"></span>
+                            @elseif ($avarage == 2 || $avarage < 3)
+                                <span class="la la-star"></span>
+                                <span class="la la-star"></span>
+                                <span class="la la-star-o"></span>
+                                <span class="la la-star-o"></span>
+                                <span class="la la-star-o"></span>
+                            @elseif ($avarage == 3 || $avarage < 4)
                                 <span class="la la-star"></span>
                                 <span class="la la-star"></span>
                                 <span class="la la-star"></span>
@@ -367,9 +374,7 @@
                         @if ($course->discount_price == NULL)
                             <p class="card-price text-black font-weight-bold">{{ $course->selling_price }} DH</p>
                         @else
-                            <p class="card-price text-black font-weight-bold">{{ $course->discount_price }} DH
-                                <span class="before-price font-weight-medium">{{ $course->selling_price }} DH</span>
-                            </p> 
+                            <p class="card-price text-black font-weight-bold">{{ $course->discount_price }} DH <span class="before-price font-weight-medium">{{ $course->selling_price }} DH</span></p> 
                         @endif
 
                         <div class="d-flex align-items-center">
@@ -401,7 +406,7 @@
         @endforelse
     </div>
 </div>
-@endforeach 
+@endforeach
 
             </div>
             <div class="more-btn-box mt-4 text-center">
